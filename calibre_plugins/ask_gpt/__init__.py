67031cc60990b72f2ad075d06ee051cf5436e2f6
Sheldonrrr <boy.liushaopeng@gmail.com>
2025-03-18 22:37:58 +0800

    delete-useless-modules

---
+++ calibre_plugins/ask_gpt/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__license__ = 'GPL v3'
+__copyright__ = '2024'
+__docformat__ = 'restructuredtext en'
+
+from calibre.customize import InterfaceActionBase
+from calibre.gui2 import get_icons
+import os
+import sys
+
+# 添加 lib 目录到 Python 路径
+lib_dir = os.path.join(os.path.dirname(__file__), 'lib')
+if lib_dir not in sys.path:
+    sys.path.insert(0, lib_dir)
+
+class AskGPTPlugin(InterfaceActionBase):
+    name                = 'Ask Grok'
+    description         = '使用 X.AI Grok 询问关于当前书籍的问题'
+    supported_platforms = ['windows', 'osx', 'linux']
+    author              = 'Sheldon'
+    version             = (1, 0, 0)
+    minimum_calibre_version = (0, 7, 53)
+    
+    # 使用 get_icons 加载图标
+    icon                = get_icons('images/ask_gpt.png', 'Ask Grok')
+    priority            = 1
+    can_be_disabled     = True
+    default_state       = True
+
+    actual_plugin = 'calibre_plugins.ask_gpt.ui:AskGPTPluginUI'
+
+    def is_customizable(self):
+        return True
+
+    def config_widget(self):
+        from calibre_plugins.ask_gpt.config import ConfigWidget
+        return ConfigWidget()
+
+    def save_settings(self, config_widget):
+        # 只保存配置，不尝试重新初始化 API
+        config_widget.save_settings()
