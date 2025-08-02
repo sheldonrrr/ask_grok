29b956f8ccb8300ece424d42939f2132b7001b78
Sheldonrrr <boy.liushaopeng@gmail.com>
2025-08-02 20:12:06 +0800

    following-error

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name=package_name,
-    version='1.1.20',
+    version='1.2.0',
     description='Ask AI about your books',
     author='Sheldon',
     author_email='sheldonrrr@gmail.com',
